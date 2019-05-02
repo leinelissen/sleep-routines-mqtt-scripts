b771daf4ec199649a2f6d755eed1fdb6aedd8f9e
Lei Nelissen <lei@codified.nl>
2019-05-02 17:13:53 +0200

    Add script for saving data

---
+++ save_data.js
@@ -0,0 +1,26 @@
+const nano = require('nano')(process.env.COUCHDB_URL);
+const mqtt = require('mqtt');
+const db = nano.db.use(process.env.COUCHDB_DB);
+
+// Create MQTT client
+var client = mqtt.connect(process.env.MQTT_HOST, {
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+});
+
+// Create handlers
+client.on('connect', function () {
+    log('Connected to MQTT server.');
+
+    // Subscribe to channel
+    client.subscribe('/sleep-routines', function(err) {
+        if (err) {
+            process.exit(1);
+        }
+    })
+});
+
+client.on('message', function(topic, data) {
+    const message = JSON.parse(data);
+    db.insert(message);
+});
